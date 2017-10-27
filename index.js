cb8cb21aa191f9d388ad3c03e0f99f72b3b33954
kaiye <catgecn@gmail.com>
2017-10-27 16:23:57 +0800

    :bug: 2.0.0 传参与判断修改

--- index.js
+++ index.js
@@ -6,22 +6,14 @@ var pngquant
 
 /**
  * 该插件会判断 build 仓库是否存在了已经编译过的图片，如果存在则跳过压缩，直接返回uid仓库里面的文件
- * 需要传参 conf 里的 pathName ，作为 release 路径的根目录
+ * 需要传参 conf 里的 distPath ，作为 release 路径的根目录
  */
 module.exports = function(content, file, conf) {
-  var targetPath = conf.pathName
+  var targetPath = conf.distPath
 
   if (targetPath) {
     var targetFile = path.join(targetPath, file.getHashRelease())
-    var exist
-
-    try {
-      exist = fs.statSync(targetFile).isFile()
-    } catch (e) {
-      exist = false
-    }
-
-    if (exist) {
+    if (fs.existsSync(targetFile)) {
       return fs.readFileSync(targetFile)
     }
   }
@@ -36,7 +28,7 @@ module.exports = function(content, file, conf) {
         fis.log.warning(
           'node-pngquant-native does not support your node ' +
             process.version +
-            ', report it to https://github.com/xiangshouding/node-pngquant-native/issues'
+            ', report it to https://github.com/mz-team/mz-optimizer-png-compress/issues'
         )
       }
     }
@@ -50,7 +42,7 @@ module.exports = function(content, file, conf) {
         fis.log.warning(
           'node-pngcrush does not support your node ' +
             process.version +
-            ', report it to https://github.com/xiangshouding/node-pngcrush/issues'
+            ', report it to https://github.com/mz-team/mz-optimizer-png-compress/issues'
         )
       }
     }
